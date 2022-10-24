8b3e00ba457152904b7a9cd1078010aadea0ab9f
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2022-10-24 13:13:17 -0700

    done with models

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,3 +1,7 @@
 export const getStudents = (req,res)=>{
   res.send('Routing is working fine at my side')
 }
+
+export const createStudent=(req,resp)=>{
+  res.send("Routing working from createStudent")
+}
