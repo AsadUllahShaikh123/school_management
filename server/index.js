6ab0203ea652717fcfa77be7672feb869a889764
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2022-10-24 12:21:51 -0700

    connected monogb and now working on routes

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({limit:'20mb',extended:true}));
+app.use(bodyParser.urlencoded({limit:'20mb',extended:true}))
+
+app.use(cors());
+
+const CONNECTION_URL =  'mongodb+srv://AsadUllah:AsadUllah0786@mern-practice.hoxmynm.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+.then(()=> app.listen(PORT,()=> 
+  console.log(`Connection is Established and running on Port :${PORT}`)
+))
+.catch((error)=> console.log(error.message) )
+
